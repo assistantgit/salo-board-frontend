6b8f87f905353a80fcaa839789d634f8bd654f80
assistantgit <kirillmer2009@gmail.com>
2026-02-23 20:41:34 +0200

    feat: створив заготівку

--- src/app/endpoint/index.tsx
+++ src/app/endpoint/index.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '../styles/globals.css'
-import { router } from '@app/routes/routes'
 import { RouterProvider } from 'react-router-dom'
+import { router } from '@app/routes/routes'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+   <RouterProvider router={router} />
   </StrictMode>,
 )
