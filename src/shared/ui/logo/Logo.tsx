6929a8c3064b74acf699b6b3ab02cdd9866c8675
_assistant <kirillmer2009@gmail.com>
2026-04-21 14:12:40 +0300

    Revert "feat: add reusable Logo component"
    This reverts commit 01122b0f9c67eddf028f91e682ed96d5720be49a.

--- src/shared/ui/logo/Logo.tsx
+++ src/shared/ui/logo/Logo.tsx
@@ -1,21 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { LogoSvg } from '@shared/assets';
 import styles from './Logo.module.css';
 
 interface LogoProps {
     className?: string;
-    to?: string;
 }
 
-export const Logo: React.FC<LogoProps> = ({ className = '', to = '/' }) => {
+export const Logo: React.FC<LogoProps> = ({ className = '' }) => {
     return (
-        <Link to={to} className={`${styles.logoWrapper} ${className}`}>
+        <div className={`${styles.logoWrapper} ${className}`}>
             <img
                 src={LogoSvg}
                 alt="Salo Board Logo"
                 className={styles.image}
             />
-        </Link>
+        </div>
     );
 };
