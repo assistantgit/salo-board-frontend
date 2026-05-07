7d12c3b390cdce745639eb5681d65e697e0cc855
_assistant <kirillmer2009@gmail.com>
2026-05-07 21:33:53 +0300

    feat: add jury tournament and submissions pages

---
+++ src/pages/jury-submissions/ui/JurySubmissionsPage.tsx
@@ -0,0 +1,11 @@
+import { Header } from '@widgets/header';
+import type React from 'react';
+import styles from './JurySubmissionsPage.module.css';
+
+export const JurySubmissionsPage: React.FC = () => {
+  return (
+    <div className={styles.page}>
+      <Header />
+    </div>
+  );
+};
---
+++ src/pages/jury-tournaments/index.ts
@@ -0,0 +1,1 @@
+export { JuryTournamentsPage } from './ui/JuryTournamentsPage';
