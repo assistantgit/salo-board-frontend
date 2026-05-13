94ef022e302be11c10916946000e0a04777da0b8
_assistant <kirillmer2009@gmail.com>
2026-05-13 19:42:11 +0300

    test: fix

--- vite.config.ts
+++ vite.config.ts
@@ -27,7 +27,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./src/app/tests/setupTests.ts'],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html', 'json-summary'],
